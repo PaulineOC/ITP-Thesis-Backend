b63f0ffebd401e2a73e602d5bdc0d051b1afe65a
PaulineOC <poc215@nyu.edu>
2022-04-14 22:54:01 -0400

    Template - more fixes - use port 8080

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter =  require('./routes/users');
 
 var app = express();
 
@@ -14,8 +15,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(cors());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
