1f5197e1121609cb21564f902b03a6dad6705637
PaulineOC <poc215@nyu.edu>
2022-04-21 15:07:52 -0400

    testing backend

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.redirect('/welcome');
+  // res.redirect('/welcome');
+  res.send("why?");
 });
 
 
