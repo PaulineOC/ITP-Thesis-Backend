9d791c18f2922172d44fc540229ab99809420fb8
PaulineOC <poc215@nyu.edu>
2022-04-21 12:10:33 -0400

    v2 backend with test routes for upload screenshots

---
+++ routes/users.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+
+router.get('/get-screenshots', function(req, res, next) {
+  /**
+   * Unity will have:
+   * UserId
+   * Array of screenshots in base64 string
+   */
+  res.send('Get screenshots correctly');
+});
+
+
+router.post('/upload-screenshots', function(req, res, next) {
+  /**
+   * Unity will have:
+   * UserId
+   * Array of screenshots in base64 string
+   */
+  res.send('Upload screenshots correctly');
+});
+
+
+module.exports = router;
