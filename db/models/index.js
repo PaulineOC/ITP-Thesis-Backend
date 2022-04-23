04cf5c4d3029a5de419a4dca7de623f68727b9e2
PaulineOC <poc215@nyu.edu>
2022-04-23 13:31:03 -0400

    use node env

--- db/models/index.js
+++ db/models/index.js
@@ -1,10 +1,12 @@
 'use strict';
 
+require('dotenv').config();
+
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV;
 const config = require(path.join(__dirname, '/../config/config.js'))[env];
 const db = {};
 
